1d6f66acf83d6b2299e56a43e0df003ef450d9c8
Jacob M <jacob.jjm.193@gmail.com>
2018-10-19 13:34:56 +1100

    finished ir

--- solutions/ir_solution/ir_solution.ino
+++ solutions/ir_solution/ir_solution.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <Zumo32U4.h>
 
+Zumo32U4LCD lcd;
+Zumo32U4Motors motors;
+Zumo32U4ProximitySensors proxSensors;
+
+void setup()
+{
+  proxSensors.initFrontSensor();
+  int front_left = 0;
+  int front_right = 0;
+  int constant_speed = 200;
+  int min_distance = 1000; 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
+  if (front_left < min_distance || front_right < min_distance)
+  {
+    motors.setSpeeds(0, 0);
+  }
+  else
+  {
+    motors.setSpeeds(constant_speed, constant_speed);
+  }
+  
+  proxSensors.read();
+  int front_left = proxSensors.countsFrontWithLeftLeds();
+  int front_right = proxSensors.countsFrontWithRightLeds();
 }
